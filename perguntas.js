6ad17226e0d34804fb5e4309b470dc497bdb7a88
lukyfierlbeck <mariane.fierlbeck@escola.pr.gov.br>
2024-10-18 11:38:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'supernatural',
+    'qual temporada o anjo castiel aparece?',
+    '4 temporada'
+)
+
+criaCartao(
+    'homem aranha',
+    'Qual ator faz o 1 homem aranha?',
+    'tobey maquire'
+)
+
+criaCartao(
+    'hora de aventura',
+    'que ano foi lançado q animação hora de aventura?',
+    '2010'
+)
+
+criaCartao(
+    'rick and morty',
+    'quantas temporadas tem a animação rick and morty?',
+    'são 7 temporadas'
+)
